e39246f334cea6535fbae737837a8bfd5d37209a
[John Moen] <[gionnym4@gmail.com]>
2022-05-24 17:54:59 -0500

    Added main route and database dependencies

--- app.py
+++ app.py
@@ -1,7 +1,49 @@
-from flask import Flask
+"""
+This is a flask app to display the data from a SQLlite database
 
+John Moen
+"""
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+"""
+Set up the Database
+"""
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# Save references to database table objects
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create a session link
+session = Session(engine)
+
+
+"""
+Set up Flask
+"""
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello world'
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!<br/>
+    Available Routes:<br/>
+    /api/v1.0/precipitation<br/>
+    /api/v1.0/stations<br/>
+    /api/v1.0/tobs<br/>
+    /api/v1.0/temp/start/end
+    ''')
+
